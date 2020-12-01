6457de9beacdb154acd9cd25a910cffc4ded76d0
Shubham Jain <shubham@sjain.dev>
2020-12-01 22:48:50 +0000

    Add hacky script to automate Notion -> GitHub Pages publishes
    This is a quick hacky script that should simplify the process of
    publishing a blog post written in Notion onto GitHub Pages.

--- README.md
+++ README.md
@@ -4,13 +4,36 @@ This repository contains the source code and blog posts for UCL COMP0016 Team 20
 
 Our team consists of Matthew Schulz, Shubham Jain and Mateusz Zielinski.
 
-## Adding a new post
+## Adding a new post (from Notion)
 
-To add a new post, follow the steps below:
+I've hacked together a quick script that should simplify publishing posts from Notion. Steps:
+
+1. Export the post from Notion as "Markdown + CSV" (no subpages)
+
+2. Unzip the exported folder and paste the contents of the unzipped folder into the [`publisher`](./publisher) folder (so you should now have a markdown file, the javascript file, and possibly a folder of images in the `publisher` folder). Don't rename anything!
+
+3. `cd publisher`
+
+4. `node publish-notion-post.js` and follow the prompts:
+
+   ```
+   > node .\publish-notion-post.js
+   Is "Week 6 Development, Login Options, and MoSCoW requ c5ac1ee480be4254983883fe20d7be7a.md" the correct blog post to publish? Y/N: Y
+   Enter the blog post's publish date in YYYY-MM-DD format: 2020-12-29
+   Enter the week number for this blog post: 10
+   Done! Double check that the images have been added under images/ and that the new post is in _posts/ with correct title, tags, date, and images!
+   ```
+
+5. Make sure everything's been added properly (in particular images), and then delete the markdown file you added in the `publisher` folder -- it will have been automatically copied to the `_posts` folder!
+
+## Adding a new post (manually)
+
+To add a new post manually, follow the steps below:
 
 1. Create a file in [`_posts/`](./_posts) with the filename in the format `YYYY-MM-DD-week-x.md`
 
 2. Add the post metadata at the beginning of the file, like below:
+
    ```
    ---
    layout: post
---
+++ publisher/publish-notion-post.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+
+// This is a quick hacky script to simplify publishing Notion markdown exports as blog posts on GitHub Pages
+// Usage: `cd publisher` followed by `node publish-notion-post.js` after pasting in the unzipped Notion export into `publisher`
+
+const fs = require('fs');
+const readline = require('readline');
+
+const reader = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+async function prompt(text) {
+  return new Promise(resolve => reader.question(`${text}: `, answer => resolve(answer)));
+}
+
+async function questionYesNo(text) {
+  const answer = await prompt(`${text} Y/N`)
+  return answer.toLowerCase().startsWith('y') ? true : false;
+}
+
+function rand(max) {
+  const number = Math.floor((Math.random() * max) + 1).toString();
+  if (number.length < 2) {
+    return number.padStart(2, '0');
+  }
+  return number;
+}
+
+(async () => {
+  const filesInDir = fs.readdirSync('.');
+  const markdownFileName = filesInDir.find(file => file.endsWith('.md'));
+
+  if (!markdownFileName) {
+    console.error('No Markdown file found in current directory');
+    return;
+  }
+
+
+  const confirmFile = await questionYesNo(`Is "${markdownFileName}" the correct blog post to publish?`);
+  if (!confirmFile) {
+    console.error('Markdown file rejected');
+    return;
+  }
+
+  const date = await prompt('Enter the blog post\'s publish date in YYYY-MM-DD format');
+  const parsedDate = new Date(date);
+  if (isNaN(parsedDate)) {
+    console.error('Invalid date');
+    return;
+  }
+
+  const weekNumber = await prompt('Enter the week number for this blog post');
+  if (weekNumber.length < 1 || !/\d+/.test(weekNumber)) {
+    console.error('Invalid week number');
+    return;
+  }
+
+  const newFilePath = `../_posts/${date}-week-${weekNumber}.md`;
+  fs.copyFileSync(markdownFileName, newFilePath);
+
+  const fileContents = fs.readFileSync(newFilePath, 'utf-8');
+  const lines = fileContents.split('\n');
+  const title = lines[0].substr(2);
+  const tagsIndex = lines.findIndex(l => l.startsWith('Tags: '));
+  const tags = tagsIndex > -1 ? lines[tagsIndex] : null;
+
+  const blogPostMetadata = {
+    layout: 'post',
+    title: `"${title}"`,
+    date: `${date} ${rand(24)}:${rand(60)}:${rand(60)} +0000`,
+    categories: tags ? tags.split('Tags: ')[1].replace(/,\s/g, ' ') : '',
+  };
+
+  // Remove first liens until tag line
+  for (let i = 0; i < (tagsIndex || 0) + 1; i++) {
+    lines.shift();
+  }
+
+  lines.unshift('---', ...Object.entries(blogPostMetadata).map(([key, value]) => `${key}: ${value}`), '---');
+
+  let imageCounter = 1;
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+    if (!(line.startsWith('!') && line.endsWith(')'))) continue;
+
+    const imagePath = line.split('(')[1].slice(0, -1).replace(/\%20/g, ' ');
+    const newImageFileName = `week-${weekNumber}-image-${imageCounter}.png`;
+    const newImageFilePath = `/blog/${newImageFileName}`;
+    imageCounter++;
+    lines[i] = `![](${newImageFilePath})`;
+
+    fs.copyFileSync(imagePath, `../images/${newImageFileName}`);
+  }
+
+  fs.writeFileSync(newFilePath, lines.join('\n'));
+
+  console.log('Done! Double check that the images have been added under images/ and that the new post is in _posts/ with correct title, tags, date, and images!');
+})();
